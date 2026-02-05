3ae26cebe1e1970784d6b3b772547ce02ffd7b79
Tushar Patel <tusharsureshpatel666@gmail.com>
2026-02-05 21:00:05 +0530

    adfs

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/Provider";
 import { ThemeProvider } from "./dashboard/components/theme/themeProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "react-hot-toast";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,7 @@ export default function RootLayout({
         >
           <Providers>
             <Toaster />
+            <SpeedInsights />
             {children}
           </Providers>
         </ThemeProvider>
